2bf880d3463b25b219d722112198d8ad5f2aaf56
R Mohan Krishna <141043010+rmohan7603@users.noreply.github.com>
2024-09-11 18:57:16 +0530

    Add files via upload

---
+++ client/src/components/ideaForm.js
@@ -0,0 +1,78 @@
+import IdeasApi from "../services/ideasApi";
+import IdeaList from "./ideaList";
+
+class IdeaForm {
+  constructor() {
+    this._formModal = document.querySelector("#form-modal");
+    this._ideaList = new IdeaList();
+  }
+
+  addEventListners() {
+    this._form.addEventListener("submit", this.handleSubmit.bind(this));
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+
+    if (
+      !this._form.elements.text.value ||
+      !this._form.elements.username.value ||
+      !this._form.elements.tag.value
+    ) {
+      alert("Please ENTER ALL FIELDS!");
+      return;
+    }
+
+    //Save user to locl storage
+    localStorage.setItem("username", this._form.elements.username.value);
+
+    const idea = {
+      username: this._form.elements.username.value,
+      text: this._form.elements.text.value,
+      tag: this._form.elements.tag.value,
+    };
+
+    //Add idea to server
+    const newIdea = await IdeasApi.createIdea(idea);
+
+    //Add  idea to ideaList
+    this._ideaList.addIdeaToList(newIdea.data.data);
+
+    //Clear fields
+    this._form.elements.text.value = "";
+    this._form.elements.username.value = "";
+    this._form.elements.tag.value = "";
+
+    this.render();
+
+    document.dispatchEvent(new Event("closemodal"));
+  }
+
+  render() {
+    this._formModal.innerHTML = `
+        <form id="idea-form">
+          <div class="form-control">
+            <label for="idea-text">Enter a Username</label>
+            <input type="text" name="username" id="username" 
+            value="${
+              localStorage.getItem('username') ?
+              localStorage.getItem('username') : ''
+            }"/>
+          </div>
+          <div class="form-control">
+            <label for="idea-text">What's Your Idea?</label>
+            <textarea name="text" id="idea-text"></textarea>
+          </div>
+          <div class="form-control">
+            <label for="tag">Tag</label>
+            <input type="text" name="tag" id="tag" />
+          </div>
+          <button class="btn" type="submit" id="submit">Submit</button>
+        </form>
+    `;
+    this._form = document.querySelector("#idea-form");
+    this.addEventListners();
+  }
+}
+
+export default IdeaForm;
